845eb75dcab28e69b6c314b1d3e60631532da3e7
joeyj <joey.jaulimsing@live.com>
2017-05-16 04:21:13 +0400

    added methods

--- src/Process/Destroy.php
+++ src/Process/Destroy.php
@@ -2,7 +2,17 @@
 
 namespace OrckidLab\VueTable\Process;
 
+use Illuminate\Support\Facades\DB;
+
 abstract class Destroy implements ProcessInterface
 {
+	public function truncate($table)
+	{
+		return DB::table($table)->truncate();
+	}
 
+	public function deleteAll($table)
+	{
+		return DB::table($table)->delete();
+	}
 }
